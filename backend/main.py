950db1bb74dc56405d3f2e34984d74988474aca8
Adwyte <148582537+adwyte@users.noreply.github.com>
2025-08-02 12:59:49 +0530

    main import fixes

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-from src.utils.config import settings
-from src.api import auth, report_issue_api
+from backend.src.utils.config import settings
+from backend.src.api import auth, report_issue_api
 
 app = FastAPI(title=settings.PROJECT_NAME)
 app.include_router(auth.router, prefix=f"{settings.API_PREFIX}/auth", tags=["auth"])
