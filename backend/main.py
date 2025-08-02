0c0c5dba129a10d42c037ca5471a62f66543f193
Adwyte <adwyte_karandikar@hyperminds.tech>
2025-08-02 12:54:18 +0530

    Backend login flow

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 
-from backend.src.api import auth
-from backend.src.utils.config import settings
-
-from backend.src.api import report_issue_api
+from src.utils.config import settings
+from src.api import auth, report_issue_api
 
 app = FastAPI(title=settings.PROJECT_NAME)
 app.include_router(auth.router, prefix=f"{settings.API_PREFIX}/auth", tags=["auth"])
