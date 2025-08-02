d889a5fb2493d7324423c6b11f43c81c704483b1
Jay Gadre <jay.gadre05@gmail.com>
2025-08-02 11:28:41 +0530

    Added Report issue and get issue api

---
+++ backend/src/api/report_issue_api.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from backend.src.api_management.report_issue_management.schemas import IssueCreate, IssueOut
+from backend.src.database import models
+from backend.src.database.database import get_db
+
+router = APIRouter()
+
+@router.post("/report-issue", response_model=IssueOut)
+def report_issue(issue: IssueCreate, db: Session = Depends(get_db)):
+    new_issue = models.Issue(**issue.dict())
+    db.add(new_issue)
+    db.commit()
+    db.refresh(new_issue)
+    return new_issue
+
+@router.get("/issues", response_model=List[IssueOut])
+def get_issues(db: Session = Depends(get_db)):
+    issues = db.query(models.Issue).order_by(models.Issue.created_at.desc()).all()
+    return issues
